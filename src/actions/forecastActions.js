ff297268e686aa1c28366d34c6772b55dc2f644c
stedice <stefanodc@mela2.fritz.box>
2017-10-14 13:19:39 +0200

    action promise

--- src/actions/forecastActions.js
+++ src/actions/forecastActions.js
@@ -3,16 +3,12 @@ import {BASE_URL, API_KEY} from '../constants/api'
 
 export const fetchForecast = () => {
   return function(dispatch) {
-    dispatch({type: "FETCH_FORECAST"});
-    
     const city ='LAquila,IT';
     const url = `${BASE_URL}?q=${city}&units=metric&appid=${API_KEY}`;
-    axios.get(url)
-      .then((response) => {
-        dispatch({type: "FETCH_FORECAST_FULFILLED", payload: response.data})
-      })
-      .catch((err) => {
-        dispatch({type: "FETCH_FORECAST_REJECTED", payload: err})
-      })
+
+    dispatch({
+      type: "FETCH_FORECAST",
+      payload: axios.get(url)
+    });
   }
 }
