0ac564da5cede2aec4d99ae1133d5aaf76f5f1df
stedice <stefanodc@mela2.fritz.box>
2017-10-14 12:09:10 +0200

    fixes

--- src/reducers/forecastReducers.js
+++ src/reducers/forecastReducers.js
@@ -1,31 +1,28 @@
 const initialState = {
   fetching: false,
   fetched: false,
-  city: {}.
+  city: {},
   forecast: [],
   error: null,
 };
 
-export default = (state=initialState, action) => {
+export default (state=initialState, action) => {
   switch (action.type) {
-    case "FETCH_USERS_PENDING": {
-      return {...state, fetching: true}
-      break;
+    case "FETCH_FORECAST_PENDING": {
+      return {...state, fetching: true};
     }
-    case "FETCH_USERS_REJECTED": {
-      return {...state, fetching: false, error: action.payload}
-      break;
+    case "FETCH_FORECAST_REJECTED": {
+      return {...state, fetching: false, error: action.payload};
     }
-    case "FETCH_USERS_FULFILLED": {
+    case "FETCH_FORECAST_FULFILLED": {
       return {
         ...state,
         fetching: false,
         fetched: true,
         city: action.payload.city,
         forecast: action.payload.list,
-      }
-      break;
+      };
     }
   }
-  return state
-}
+  return state;
+};
