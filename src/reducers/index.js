c10b0cfae10c982dcd4b01f730d3ec1a54d02950
stedice <stefanodc@mela2.fritz.box>
2017-10-15 21:15:36 +0200

    new reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 // Set up your root reducer here...
 import { combineReducers } from 'redux';
 import forecastReducers from './forecastReducers';
+import dayReducers from './dayReducers';
 
 export default combineReducers({
 	forecastReducers: forecastReducers,
+	dayReducers: dayReducers,
 });
